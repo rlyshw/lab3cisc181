f46c0bd73f325d9ea4a1a67da26c88377ba07286
Tommy Kananen <tkananen@udel.edu>
2015-03-24 22:18:33 -0400

    :/

---
+++ CISC181.PokerEngine/src/poker/PokerJUnit.java
@@ -0,0 +1,39 @@
+package poker;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PokerJUnit {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void flushHandtest() {
+		ArrayList<Hand> flushHand = new ArrayList<Hand>();
+	
+		
+		
+		
+	}
+}
